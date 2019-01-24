1210fb8fcce37f3a7289fa5968fdd29b7a6617e7
Remi Jonkman <rb.jonkman@gmail.com>
2019-01-24 14:27:54 +0100

    Uploaded assignment sources and files

--- src/Polynomial.hs
+++ src/Polynomial.hs
@@ -2,19 +2,25 @@ module Polynomial where
 
 import Clash.Prelude
 
+-- type System is defined in 'Clash.Explicit.Signal' as: Dom "system" 10000
+-- Hence System is a domain identified by 'system' and clock period 10ns
+-- Ungated (due to Source) system (read domain) clock
 type Clk = Clock System Source
+
+-- Asynchronous reset for system domain
 type Rst = Reset System Asynchronous
 type Sig = Signal System
 
 type Value = Signed 16
+type Vector = Vec 3 Value
 
 -- Student information:
 --  Student 1
---    lastname:
---    student number:
+--    Pereira
+--    s
 --  Student 2
---    lastname:
---    student number:
+--    Jonkman
+--    s1563599
 
 
 -- NOTE: topentity functions are at the bottom
@@ -24,36 +30,53 @@ type Value = Signed 16
 -- Assignment 1
 -- f0p f0m
 -----------------------------------------------------------
+f0p :: Value -> Value
+f0p x = x^4 + 22*x^3 + 172*x^2 + 552*x + 576
 
+f0m :: Value -> Value
+f0m x = x*x*x*x + 22*x*x*x + 172*x*x + 552*x + 576
+
+f0mb :: Value -> Value
+f0mb x = (x*x*x*x) + 22*(x*x*x) + 172*(x*x) + 552*x + 576
 -----------------------------------------------------------
 -- Assignment 2
 -- f0f
 -----------------------------------------------------------
-
+f0f :: Value -> Value
+f0f x = (x + 2) * (x + 6) * (x + 6) * (x + 8)
 -----------------------------------------------------------
 -- Assignment 3
 -- f0hof
 ----------------------------------------------------------
+helper x y z = y*(x+z)
+
+--f0hof :: Value -> Value
+f0hof x = foldl (helper x) ((2+x) :: Value) (6:>6:>8:>Nil)
 
 -----------------------------------------------------------
 -- Assignment 4
 -- f0mealy
 ----------------------------------------------------------
+f0helper s (x, y) = (s', z)
+  where
+    s'= z*(x+y)
+    z = s'
+
+f0mealy x = mealy f0helper 1 x
 
 -----------------------------------------------------------
 -- topEntity's
 -----------------------------------------------------------
 
 -- Assignment 1
---topEntity hs x = (f0m hs x, f0p hs x)
+--topEntity = f0p
 
 -- Assignment 2
---topEntity hs x = f0f hs x
+--topEntity = f0f
 
 -- Assignment 3
---topEntity hs x = f0hof hs x
+--topEntity = f0hof
 
 -- Assignment 4
---topEntity :: Clk -> Rst -> Value -> Sig Value -> Sig Value
---topEntity clk rst x = exposeClockReset (f0mealy x) clk rst
-
+topEntity :: Clk -> Rst -> Sig (Value, Value) -> Sig Value
+topEntity clk rst x = exposeClockReset f0mealy clk rst x
