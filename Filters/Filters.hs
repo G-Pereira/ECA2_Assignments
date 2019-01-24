09c0e94a8270731dfffed8619e68ea341293285d
Remi Jonkman <rb.jonkman@gmail.com>
2019-01-24 16:15:43 +0100

    Added all projects

---
+++ Filters/Filters.hs
@@ -0,0 +1,103 @@
+module Filters where
+
+import Clash.Prelude
+import FilterCoefAndInput
+
+type Clk = Clock System Source
+type Rst = Reset System Asynchronous
+type Sig = Signal System
+
+-- Student information:
+--  Student 1
+--    lastname:
+--    student number:
+--  Student 2
+--    lastname:
+--    student number:
+
+-- NOTE: topentity functions are at the bottom
+-- comment and uncomment the functions for each assignment
+-- it should not be necessary, but you may change the 
+-- definition of the topEntity functions.
+
+-----------------------------------------------------------
+-- Assignment 1
+-- FIR1 N = 6 
+-----------------------------------------------------------
+
+-----------------------------------------------------------
+-- Assignment 2
+-- FIR1 N = 100 
+-----------------------------------------------------------
+
+-----------------------------------------------------------
+-- Assignment 3
+-- FIR2 N = 6
+-----------------------------------------------------------
+
+-----------------------------------------------------------
+-- Assignment 4
+-- FIR2 N = 100
+-----------------------------------------------------------
+
+-----------------------------------------------------------
+-- Assignment 5
+-- FIR3 N = 6
+-----------------------------------------------------------
+
+-----------------------------------------------------------
+-- Assignment 6
+-- FIR3 N = 100
+-----------------------------------------------------------
+
+-----------------------------------------------------------
+-- Assignment 7
+-- FIR3' N = 6
+-----------------------------------------------------------
+
+-----------------------------------------------------------
+-- Assignment 8
+-- IIR
+-----------------------------------------------------------
+
+-----------------------------------------------------------
+-- Assignment 9
+-- IIR
+-----------------------------------------------------------
+
+-----------------------------------------------------------
+-- topEntity's
+-----------------------------------------------------------
+-- Assignment 1
+--topEntity = fir1_6
+
+-- Assignment 2
+--topEntity = fir1_100
+
+-- Assignment 3
+--topEntity :: Clk -> Rst -> Sig (Signed 8) -> Sig (Signed 8)
+--topEntity clk rst = exposeClockReset fir2_6 clk rst
+
+-- Assignment 4
+--topEntity :: Clk -> Rst -> Sig (SFixed 5 13) -> Sig (SFixed 5 13)
+--topEntity clk rst = exposeClockReset fir2_100 clk rst
+
+-- Assignment 5
+--topEntity :: Clk -> Rst -> Sig (Signed 8) -> Sig (Signed 8)
+--topEntity clk rst = exposeClockReset fir3_6 clk rst
+
+-- Assignment 6
+--topEntity :: Clk -> Rst -> Sig (SFixed 5 13) -> Sig (SFixed 5 13)
+--topEntity clk rst = exposeClockReset fir3_100 clk rst
+
+-- Assignment 7
+--topEntity :: Clk -> Rst -> Sig (Signed 8) -> Sig (Signed 8)
+--topEntity clk rst = exposeClockReset fir3t_6 clk rst
+
+-- Assignment 8
+--topEntity :: Clk -> Rst -> Sig (SFixed 5 13) -> Sig (SFixed 5 13)
+--topEntity clk rst = exposeClockReset iir1 clk rst
+
+-- Assignment 9
+--topEntity :: Clk -> Rst -> Sig (SFixed 5 13) -> Sig (SFixed 5 13)
+--topEntity clk rst = exposeClockReset iir2 clk rst
---
+++ Polynomial/Polynomial.hs
@@ -0,0 +1,82 @@
+module Polynomial where
+
+import Clash.Prelude
+
+-- type System is defined in 'Clash.Explicit.Signal' as: Dom "system" 10000
+-- Hence System is a domain identified by 'system' and clock period 10ns
+-- Ungated (due to Source) system (read domain) clock
+type Clk = Clock System Source
+
+-- Asynchronous reset for system domain
+type Rst = Reset System Asynchronous
+type Sig = Signal System
+
+type Value = Signed 16
+type Vector = Vec 3 Value
+
+-- Student information:
+--  Student 1
+--    Pereira
+--    s
+--  Student 2
+--    Jonkman
+--    s1563599
+
+
+-- NOTE: topentity functions are at the bottom
+-- comment and uncomment the functions for each assignment
+
+-----------------------------------------------------------
+-- Assignment 1
+-- f0p f0m
+-----------------------------------------------------------
+f0p :: Value -> Value
+f0p x = x^4 + 22*x^3 + 172*x^2 + 552*x + 576
+
+f0m :: Value -> Value
+f0m x = x*x*x*x + 22*x*x*x + 172*x*x + 552*x + 576
+
+f0mb :: Value -> Value
+f0mb x = (x*x*x*x) + 22*(x*x*x) + 172*(x*x) + 552*x + 576
+-----------------------------------------------------------
+-- Assignment 2
+-- f0f
+-----------------------------------------------------------
+f0f :: Value -> Value
+f0f x = (x + 2) * (x + 6) * (x + 6) * (x + 8)
+-----------------------------------------------------------
+-- Assignment 3
+-- f0hof
+----------------------------------------------------------
+helper x y z = y*(x+z)
+
+--f0hof :: Value -> Value
+f0hof x = foldl (helper x) ((2+x) :: Value) (6:>6:>8:>Nil)
+
+-----------------------------------------------------------
+-- Assignment 4
+-- f0mealy
+----------------------------------------------------------
+f0helper s (x, y) = (s', z)
+  where
+    s'= z*(x+y)
+    z = s'
+
+f0mealy x = mealy f0helper 1 x
+
+-----------------------------------------------------------
+-- topEntity's
+-----------------------------------------------------------
+
+-- Assignment 1
+--topEntity = f0p
+
+-- Assignment 2
+--topEntity = f0f
+
+-- Assignment 3
+--topEntity = f0hof
+
+-- Assignment 4
+topEntity :: Clk -> Rst -> Sig (Value, Value) -> Sig Value
+topEntity clk rst x = exposeClockReset f0mealy clk rst x
